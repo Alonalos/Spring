b80beac35b8aea452980c373d678c1dfbe8dfabd
Alonalos <alonalos8@gmail.com>
2020-10-29 14:47:13 -0400

    PostConstruct, PreDestroy

---
+++ Spring-06-javaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/config/CybertekAppConfig.java
@@ -0,0 +1,13 @@
+package com.cybertek.config;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cybertek")
+public class CybertekAppConfig {
+
+}
---
+++ Spring-06-javaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,23 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+
+@Component
+public class Java implements Course {
+    public void getTeachingHours() {
+        System.out.println("Teaching hours: 5");
+    }
+    @PostConstruct
+    public void postConstructor(){
+        System.out.println("Executing post constructor");
+    }
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("Executing pre destroy");
+    }
+}
