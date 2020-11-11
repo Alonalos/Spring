6566958fcfffc69616cc2c86e45da183d62af4b7
Alonalos <alonalos8@gmail.com>
2020-11-10 21:20:28 -0500

    MVC

---
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -0,0 +1,55 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping
+    public String getRequestMapping(){
+
+        return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "aliona")
+    public String getRequestMapping2(){
+
+        return "home";
+    }
+
+    @RequestMapping("champ")
+    public String getRequestMapping3(){
+
+        return "home";
+    }
+    @GetMapping("/spring")
+    public String getMappingEx(){
+        return "home";
+    }
+
+    @PostMapping("/spring")
+    public String postMappingEx(){
+        return "home";
+    }
+
+    @GetMapping("/home/{name}")
+    public String pathVariableEx(@PathVariable("name") String name){
+        System.out.println("name is: "+name);
+        return "home";
+    }
+
+    @GetMapping("/home/{name}/{email}")
+    public String pathVariableEx2(@PathVariable("name") String name,@PathVariable("email")String email){
+        System.out.println("name is: "+name);
+        System.out.println("email is: "+email);
+        return "home";
+    }
+
+    @GetMapping("/home/course")
+    public String requestParamEx(@RequestParam("course") String course){
+        System.out.println("name is: "+course);
+        return "home";
+    }
+
+}
