7f3501e878213d70308a49c938d781edbfd36ec1
Alonalos <alonalos8@gmail.com>
2020-11-23 16:04:42 -0500

    Form

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Employee.java
@@ -0,0 +1,24 @@
+package com.cybertek.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String birthday;
+    private String password;
+    private String address;
+    private String address2;
+    private String state;
+    private String zipCode;
+    private String city;
+
+
+}
