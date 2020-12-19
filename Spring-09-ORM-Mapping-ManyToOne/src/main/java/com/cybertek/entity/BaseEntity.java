983d426c2af47ca4e479d29a5160de29c0e59fa1
Alonalos <alonalos8@gmail.com>
2020-12-18 20:18:52 -0500

    ORM Mapping Many To One

---
+++ Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/entity/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.cybertek.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
