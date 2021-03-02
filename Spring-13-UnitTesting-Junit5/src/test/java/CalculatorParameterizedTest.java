62a24a0a9fa6076dcf4dd9c6de8272f52d87f3aa
Alonalos <alonalos8@gmail.com>
2021-03-02 10:55:43 -0500

    Unit Testing - Junit5

--- Spring-13-UnitTesting-Junit5/src/test/java/CalculatorParameterizedTest.java
+++ Spring-13-UnitTesting-Junit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
+import org.junit.platform.engine.support.descriptor.FileSource;
 
 public class CalculatorParameterizedTest {
 
@@ -34,5 +35,20 @@ public class CalculatorParameterizedTest {
     static String[] stringProvider(){
         String arr[]={"Java","JS","TS"};
         return arr;
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "10,20,30",
+            "20,30,50",
+            "30,40,30"
+    })
+    void testCase5(int num1,int num2,int result){
+        Assertions.assertEquals(result,Calculator.add(num1,num2));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources="/sample-data.csv",numLinesToSkip = 1)
+    void testCase6(int num1,int num2,int result){
+        Assertions.assertEquals(result,Calculator.add(num1,num2));
     }
 }
